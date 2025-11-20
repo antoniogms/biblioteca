6962f1591eb05d529034506c9ba41876a2442d81
antoniogms <antonio.gms1998@gmail.com>
2025-11-20 18:38:02 -0300

    Adicionado funcionalidade Gerenciar Autor

--- controller/autor_controller.py
+++ controller/autor_controller.py
@@ -1,6 +1,47 @@
+from model.autor_model import AutorModel
+from view.autor_view import AutorView
+
 class AutorController:
     def __init__(self):
-        pass
+        self.model = AutorModel()
+        self.view = AutorView()
 
     def executar(self):
-        print("Test")
+        while True:
+            opcao = self.view.menu()
+
+            if opcao == 1:
+                nome, nacionalidade = self.view.cadastrar()
+
+                if nome and nacionalidade:
+                    self.model.inserir(nome, nacionalidade)
+                    self.view.mensagem("\n Autor cadastrado com sucesso!\n")
+
+            elif opcao == 2:
+                autores = self.model.listar()
+                self.view.listar(autores)
+                
+            elif opcao == 3:
+                id_autor, nome, nacionalidade = self.view.atualizar()
+                if id_autor and nome and nacionalidade:
+                    sucesso = self.model.atualizar(id_autor, nome, nacionalidade)
+                    if sucesso:
+                        self.view.mensagem("\nAutor atualizado com sucesso!\n")
+                    else:
+                        self.view.mensagem("\nFalha ao atualizar: ID não encontrado.\n")
+                        
+            elif opcao == 4:
+                id_autor = self.view.excluir()
+                if id_autor:
+                    sucesso = self.model.excluir(id_autor)
+                    if sucesso:
+                        self.view.mensagem("\nAutor excluído com sucesso!\n")
+                    else:
+                        self.view.mensagem("\nFalha aoo excluir: ID não encontrado.\n")
+
+            elif opcao == 5:
+                self.view.mensagem("\nVoltand ao menu principal...\n")
+                break
+
+            else:
+                self.view.mensagem("\nOpção inválida!\n")
--- view/menu_view.py
+++ view/menu_view.py
@@ -1,13 +1,13 @@
 class MenuView:
     @staticmethod
     def menu():
-        print("\n--------- MENU DE PRODUTOS ---------\n")
+        print("\n--------- MENU PRINCIPAL ---------\n")
         print("1. Gerenciar autor")
         print("2. Gerenciar livro")
         print("3. Sair")
 
         try:
-            return int(input("Escolha uma opção: "))
+            return int(input("\nEscolha uma opção: "))
         except ValueError:
             print("\nEntrada inválida. Digite um número.\n")
             return -1
