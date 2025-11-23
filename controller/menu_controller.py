d5a0f201ebe6622c994e7ba622e6d4b85bd9c9c0
antoniogms <antonio.gms1998@gmail.com>
2025-11-22 21:04:43 -0300

    Reiniciando trabalho na branche

--- controller/menu_controller.py
+++ controller/menu_controller.py
@@ -1,17 +1,15 @@
 from view.menu_view import MenuView
-from controller.autor_controller import AutorController
 
 class MenuController:
     def __init__(self):
         self.view = MenuView()
-        self.controller = AutorController()
-
+        
     def executar(self):
         while True:
             opcao = self.view.menu()
 
             if opcao == 1:
-                self.controller.executar()
+                pass
 
             elif opcao == 2:
                 pass
